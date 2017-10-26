c62c4161dfbd7990fa4303062c8c1e585d2a8d0d
Bryan Lian <bryan.lian.2014@sis.smu.edu.sg>
2017-10-26 14:08:59 +0800

    Edited main.ui and main.py so that it will work HAHHA

--- Main.py
+++ Main.py
@@ -7,14 +7,14 @@ import StockReader
 qtCreatorFile = "Main.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
-class Main(QMainWindow, Ui_MainWindow):          
+class Main(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.searchButton.clicked.connect(self.test)
-        
-    def test():
-        pass
+
+    def test(self):
+        print("test")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
