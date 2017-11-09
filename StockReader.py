f3d4c50ecdfe2f2a25ff4bd921053db676240bcc
Bryan Lian <bryan.lian.2014@sis.smu.edu.sg>
2017-11-09 16:00:34 +0800

    add covariance, portfolio risk and correlation

--- StockReader.py
+++ StockReader.py
@@ -5,8 +5,9 @@ Created on Mon Oct  9 22:21:39 2017
 @author: shtang.2014
 """
 import pandas_datareader.data as pdr
-import numpy as np 
+import numpy as np
 import quandl
+import cmath as math
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from CustomExceptions import *
@@ -21,25 +22,25 @@ def getStock(stock):
     #print (today)
     #print(three_yrs_ago)
     #stock=pdr.get_data_google('AAPL',three_yrs_ago, today)
-    
+
     #EXAMPLE- retrieve data for Apple
-    ##df = pdr.DataReader(stock, 'yahoo', three_yrs_ago, today) 
-    
+    ##df = pdr.DataReader(stock, 'yahoo', three_yrs_ago, today)
+
     df = getStockDF(stock)
     #    print(df)
-    
+
     returnList = []
 
-    #get a list of close values within the time period for the calculation for Sharpe ratio 
-    #calculate the average returns based on the current day's price and the previous day price 
-    for i in range(len(list(df['Close']))): 
+    #get a list of close values within the time period for the calculation for Sharpe ratio
+    #calculate the average returns based on the current day's price and the previous day price
+    for i in range(len(list(df['Close']))):
         if(i < len(list(df['Close'])) - 1):
             first = list(df['Close'])[i]
             second = list(df['Close'])[i + 1]
-            averageReturn = (second - first) / first 
+            averageReturn = (second - first) / first
             returnList.append(averageReturn)
-        
-    global stockGlobalMap #declaring it global to update the list for access 
+
+    global stockGlobalMap #declaring it global to update the list for access
     stockGlobalMap.update({stock:returnList})
 #    stockGlobalMap[stock] = returnList
     return returnList;
@@ -48,7 +49,7 @@ def getStockDF(stock):
     today = datetime.now()
     noOfYears = 1
     one_year_ago = datetime.now() - relativedelta(years=noOfYears)
-    
+
     #EXAMPLE- retrieve data for Apple
 #    df = pdr.DataReader(stock, 'yahoo', one_year_ago, today)
     try:
@@ -56,20 +57,50 @@ def getStockDF(stock):
     except RemoteDataError:
         raise ApplicationException('Too many API requests','')
     return df
-          
+
 def sharpe(returns):
     """
     Sharpe ratio = (Mean Return - Risk Free Rate)/(Risk)
-    Risk is calculated using Standard Deviation. Essentially, sharpe ratio is interpreted as amount of return per unit of risk. 
-    The higher the sharpe ratio, the better. 
+    Risk is calculated using Standard Deviation. Essentially, sharpe ratio is interpreted as amount of return per unit of risk.
+    The higher the sharpe ratio, the better.
     Risk free rate is based on US 1-year treasury bill.
-    252 is the number of trading days in a year for annualisation 
+    252 is the number of trading days in a year for annualisation
     """
     Mean_Return = (np.mean(returns))*100*252
     rf = quandl.get("FRED/TB1YR", authtoken="VV_5NSuUzyPxy8sgZkzp")
     Risk_Free = (rf['Value'][-1])
     Risk = (np.std(returns))*100*((252)**0.5)
     return (Mean_Return - Risk_Free)/(Risk)
+def covMatrix(stockList):
+    if(len(stockList)>2):
+        a = getStock(stockList[0])
+        b = getStock(stockList[1])
+        vStackArray = np.vstack((a,b))
+        for i in range(2,len(stockList))):
+            currentStock = getStock(stockList[i])
+            vStackArray = np.vstack((vStackArray,currentStock))
+    return np.cov(vStackArray,ddof=0)
+
+def portfolioRisk(matrix,weightage):
+    result = 0.0
+    for a in range(0,len(matrix)):
+        for b in range (0,a+1):
+            if(b==a):
+                result += weightage[b]**2*matrix[a,b]
+            else:
+                result += matrix[a,b]*weightage[a]*weightage[b]*2
+    return math.sqrt(result)
+
+def portfolioCorrelation(matrix,weightage,risk):
+    result=risk**2
+    bottom=0.0
+    for a in range (0,len(matrix)):
+        result -= (weightage[a]**2)*(matrix[a,a])
+        for b in range (0,a):
+            bottom += weightage[a]*weightage[b]*math.sqrt(matrix[a,a])*math.sqrt(matrix[b,b])*2
+    result = result/bottom
+    print(type(result))
+    return result
 
 #simple calculation for double checking
 #stockList = getStock("AAPL")
@@ -79,5 +110,3 @@ def sharpe(returns):
 
 #value = sharpe(stockList)
 #print("sharpe ratio:", value)
-    
-
