44934ebb4489209c60e68b8919d8a6a94a3ecde1
LU JIANAN <jianan.lu.2015@smu.edu.sg>
2017-11-09 13:28:16 +0800

    Update validation.py

--- Validation.py
+++ Validation.py
@@ -1,4 +1,5 @@
 class Validation(Object){
+	
 	def PB_C(self):
 	
 		# valid number input
@@ -22,8 +23,6 @@ class Validation(Object){
 			print ("Please enter a valid number!")
 			number = False
 	
-	
-	
 		# valid string input
 		word = str(self.input())
 		
@@ -31,7 +30,38 @@ class Validation(Object){
 			word = False
 		else:
 			word = True
+	
+	# valid string input
+	def validString(word):
+		
+		if not word.isalpha():
+			word = False
+		else:
+			word = True
+	
+	
+	# valid number input
+	def validNumber(x):
+		try:
+			# for integer input			
+			number =  int(self.input())
 			
+			# for double or float number input
+			#number = float(self.input())
+			#number = double(self.input())
+			
+			# check the number is not zero or negative
+			if number <= 0:			
+				print("Please enter a valid number!")
+				number = False # if 
+			
+			# if passed validation, return True
+			number = True
+			
+		except ValueError:
+			print ("Please enter a valid number!")
+			number = False
+	
 	
 	# check for input file but not sure whether it can work or not 
 	def validFile(x):
@@ -41,10 +71,13 @@ class Validation(Object){
 			value = int(read.strip())
 		except IOError as eer:
 			print("Wrong File Format!")
+			return False
 		except ValueError:
 			print("Could not convert data to an integer.")
+			return False
 		finally:
 			x.close()
+			return True
 	
 	# check or zeroDivisionError
 	def vaildCalculation(x):
@@ -53,5 +86,18 @@ class Validation(Object){
 			# y = x/1
 		except ZeroDivisionError as err:
 			print('Handling run-time calculation error:',err)
-	
+			
+	def validUrl(url):
+		try:
+			r = requests.get(url, params={'s': thing})
+		except requests.exceptions.Timeout as err:
+			# Maybe set up for a retry, or continue in a retry loop
+			print('Oops!There is a loop inside the program, request time out!')
+		except requests.exceptions.TooManyRedirects as err:
+			# Tell the user their URL was bad and try a different one
+			print('The url redirect too many times')
+		except requests.exceptions.RequestException as err:
+			# catastrophic error. bail.
+			print err
+			sys.exit(1)
 }
